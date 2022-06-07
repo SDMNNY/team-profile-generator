8732dcc268557ee074a917bee0aea2c7021408ee
sdmnny <sdmnny@gmail.com>
2022-06-07 19:09:31 -0400

    index contains html template

--- index.js
+++ index.js
@@ -82,3 +82,83 @@ const addEmployee = () => {
               });
           });
       };
+      const generateEmployeeCard = (employee) => {
+        let roleContent;
+        switch (employee.getRole()) {
+          case "Manager":
+            roleContent = `Office Number: ${employee.officeNumber()}`;
+            break;
+          case "Engineer":
+            roleContent = `GitHub: <a href="https://github.com/${employee.getGithub()}" target="blank">${employee.getGithub()}</a>`;
+            break;
+          case "Intern":
+            roleContent = `School Name: ${employee.getSchool()}`;
+            break;
+        }
+        return `
+              <div class="card text-white bg-danger mb-5 mr-4 col-sm-1 col-md-4 col-lg-4" style="max-width: 18rem">
+                <div class="card-header">
+                  <h2>${employee.getName()}</h2>
+                  <h3>${employee.getRole()}</h3>
+                </div>
+                <div class="card-body">
+                  <ul class="list-group">
+                    <li class="list-group-item bg-danger param">ID: ${employee.getId()}</li>
+                    <li class="list-group-item bg-danger param">Email: <a href="mailto:${employee.getEmail()}">${employee.getEmail()}</a></li>
+                    <li class="list-group-item bg-danger param">${roleContent}</li>
+                  </ul>
+                </div>
+              </div>`;
+      };
+      const renderEmployeeCard = (teamArray) => {
+        const htmlCards = [];
+        teamArray.forEach((employee) => {
+          htmlCards.push(generateEmployeeCard(employee));
+        });
+        return htmlCards.join("");
+      };
+      
+      const generateHTML = (teamArray) => {
+        return `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>Team Profile Generator! | Quick access to your employee info</title>
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+            integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn"
+            crossorigin="anonymous"/>
+          <link rel="stylesheet" href="../assets/css/styles.css" />
+        </head>
+        <body class="bg-dark">
+          <header>
+            <div class="jumbotron jumbotron-fluid bg-danger text-white">
+              <div class="container">
+                <h1 class="display-4 text-center">My Team!</h1>
+                <p class="lead text-center">
+                  Readily displays your team information.
+                </p>
+              </div>
+            </div>
+          </header>
+          <main class="container row m-auto d-flex justify-content-around align-items-center">
+                  ${renderEmployeeCard(teamArray)}
+          </main>
+        </body>
+      </html>
+      `;
+      };
+      
+      const writeToFile = (fileName, data) => {
+        fs.writeFile(fileName, data, (err) => {
+          err
+            ? console.error(`Failed to create HTML file: ${err}`)
+            : console.log("Successfully created your HTML File!");
+        });
+      };
+      
+      init();
